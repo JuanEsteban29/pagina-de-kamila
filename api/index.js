a8af454ad8b008abab1e22d88e917f9eb5e5ad97
juan <juanbri2905@gmail.com>
2026-08-22 17:14:16 -0400

    Fix Vercel Serverless Function entrypoint and route rewrites for /api/*

--- api/index.js
+++ api/index.js
@@ -1,3 +1,9 @@
 const app = require('../server.js');
 
-module.exports = app;
+module.exports = (req, res) => {
+    // Normalizar req.url para que Vercel Serverless siempre coincida con las rutas de Express
+    if (req.url && !req.url.startsWith('/api')) {
+        req.url = '/api' + (req.url.startsWith('/') ? req.url : '/' + req.url);
+    }
+    return app(req, res);
+};
